3046eb5f63484c798f34902733b9553f0224c371
white-letter <121116111@qq.com>
2022-04-06 23:27:47 +0800

    feat: lengthOfLIS

---
+++ algorithm/__test__/lengthOfLIS.test.js
@@ -0,0 +1,10 @@
+import { lengthOfLIS } from '../lengthOfLIS';
+
+
+test('lengthOfLIS',()=>{
+
+  const numList  = [10,9,2,5,3,7,101,18]
+
+  const result = lengthOfLIS(numList)
+  expect(result).toBe(4)
+})
---
+++ algorithm/lengthOfLIS.ts
@@ -0,0 +1,12 @@
+export function lengthOfLIS(numList: number[]) {
+  const result = new Array<number>(numList.length).fill(1);
+  for (let i = 0; i < numList.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (numList[j] < numList[i]) {
+        result[i] = Math.max(result[i], result[j] + 1);
+      }
+    }
+  }
+  console.log(result);
+  return Math.max(...result);
+}
