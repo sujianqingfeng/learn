1afeac1f54da8cc8b7592536b14bd7c1352f6b04
white-letter <121116111@qq.com>
2022-04-14 21:42:05 +0800

    feat: two-sum

---
+++ algorithm/hot100/1-two-sum/__test__/two-sum.test.js
@@ -0,0 +1,16 @@
+import { twoSum } from "../index";
+
+describe("twoSum", () => {
+  it("1", () => {
+    const numList = [3, 2, 4];
+    const result = twoSum(numList, 6);
+    expect(result).toEqual([1, 2]);
+  });
+
+
+  it("2", () => {
+    const numList = [2,7,11,15];
+    const result = twoSum(numList, 18);
+    expect(result).toEqual([1, 2]);
+  });
+});
