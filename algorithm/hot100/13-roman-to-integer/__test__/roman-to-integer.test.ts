ecfee688ac06e3aca9abb9197f4a501adf1b370e
hens <121116111@qq.com>
2022-05-05 22:38:30 +0800

    feat: roman-to-integer

---
+++ algorithm/hot100/13-roman-to-integer/__test__/roman-to-integer.test.ts
@@ -0,0 +1,20 @@
+import { describe, it, expect } from "vitest";
+
+import { romanToInt } from "../index";
+
+describe("integer-to-roman", () => {
+  it("1", () => {
+    const s = 'MCMXCIV' 
+    const res= 1994 
+    const result = romanToInt(s) 
+    expect(result).toBe(res);
+  });
+
+
+  it("2", () => {
+    const s = 'X'
+    const res= 10
+    const result = romanToInt(s) 
+    expect(result).toBe(res);
+  });
+});
---
+++ algorithm/hot100/13-roman-to-integer/index.ts
@@ -0,0 +1,31 @@
+
+export function romanToInt(s:string){
+
+
+  const map:Record<string,number> = {
+    I:1,
+    V:5,
+    X:10,
+    L:50,
+    C:100,
+    D:500,
+    M:1000
+  }
+
+
+  let i = 0,res = 0
+  const l = s.length
+
+  while(i<l){
+    const cur = map[s[i]]
+    if(i< l-1 && cur< map[s[i+1]]){
+      res-=cur
+    }else{
+      res+=cur
+    }
+    // console.log(cur,res);
+    i++
+  }
+
+  return res
+}
