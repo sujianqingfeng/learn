01fb6860515c8cea41a7402ccfacc557840e56d5
sujianqingfeng <121116111@qq.com>
2022-07-13 20:20:14 +0800

    feat: 553-optimal-division

---
+++ algorithm/553-optimal-division/index.ts
@@ -0,0 +1,19 @@
+// 553. 最优除法
+export function optimalDivision(nums: number[]) {
+  const n = nums.length
+  const ans = []
+
+  for (const num of nums) {
+    ans.push(num)
+    ans.push("/")
+  }
+  ans.pop()
+
+  if (n > 2) {
+    const index = ans.findIndex((item) => item === "/")
+    ans.splice(index + 1, 0, "(")
+    ans.push(")")
+  }
+
+  return ans.join("")
+}
