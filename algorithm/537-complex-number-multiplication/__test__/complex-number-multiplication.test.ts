9d93faaa89607565270fd60b69e9ce3b09ca1222
hens <121116111@qq.com>
2022-07-14 21:19:16 +0800

    feat: 537-complex-number-multiplication

---
+++ algorithm/537-complex-number-multiplication/__test__/complex-number-multiplication.test.ts
@@ -0,0 +1,12 @@
+import { describe, it, expect } from "vitest"
+
+import { complexNumberMultiply } from "../index"
+
+describe("minimum-time-difference", () => {
+  it("1", () => {
+    const num1 = "1+1i",
+      num2 = "1+1i"
+    const result = complexNumberMultiply(num1, num2)
+    expect(result).toEqual("0+2i")
+  })
+})
---
+++ algorithm/537-complex-number-multiplication/index.ts
@@ -0,0 +1,14 @@
+// 537. 复数乘法
+export function complexNumberMultiply(num1: string, num2: string) {
+  const getComplex = (str: string) => {
+    const t = str.split("+")
+    return [+t[0], +t[1].replace("i", "")]
+  }
+
+  const complex1 = getComplex(num1)
+  const complex2 = getComplex(num2)
+
+  return `${complex1[0] * complex2[0] - complex1[1] * complex2[1]}+${
+    complex1[0] * complex2[1] + complex1[1] * complex2[0]
+  }i`
+}
