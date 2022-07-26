902da46fb83c089e2cf3d0a73fd0a17a8d2fcb1d
sujianqingfeng <121116111@qq.com>
2022-07-26 19:16:51 +0800

    feat: 415-add-strings

---
+++ algorithm/415-add-strings/__test__/add-strings.test.ts
@@ -0,0 +1,19 @@
+import { describe, it, expect } from "vitest"
+
+import { addString } from "../index"
+
+describe("add-strings", () => {
+  it("1", () => {
+    const num1 = "456",
+      num2 = "77"
+    const result = addString(num1, num2)
+    expect(result).toEqual("533")
+  })
+
+  it("2", () => {
+    const num1 = "1",
+      num2 = "9"
+    const result = addString(num1, num2)
+    expect(result).toEqual("10")
+  })
+})
