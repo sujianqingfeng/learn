1961a459e306acac511abde5729e83c24caf2ff7
sujianqingfeng <121116111@qq.com>
2022-08-09 10:17:50 +0800

    feat: iterator

---
+++ design-patterns/iterator/__test__/iterator.test.ts
@@ -0,0 +1,18 @@
+import { describe, it, expect } from "vitest"
+
+import { range } from "../index"
+
+describe("iterator", () => {
+  it("1", () => {
+    const result = []
+    for (const n of range(1, 10, 5)) {
+      result.push(n)
+    }
+    expect(result).toMatchInlineSnapshot(`
+      [
+        1,
+        6,
+      ]
+    `)
+  })
+})
