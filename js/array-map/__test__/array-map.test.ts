1e5876e30b9122887f61d3af7e2bca710e4dc683
sujianqingfeng <121116111@qq.com>
2022-08-09 14:11:44 +0800

    chore: array-map

---
+++ js/array-map/__test__/array-map.test.ts
@@ -0,0 +1,11 @@
+import { describe, test, expect } from "vitest"
+
+import map from ".."
+
+describe("array map", () => {
+  test("test array", () => {
+    const array = [1, 2]
+    const newArray = map.call(array, (item: any) => item + 1)
+    expect(newArray).toEqual([2, 3])
+  })
+})
