89c7b3662814bbad5c6eae5c23548972f1256a70
hens <121116111@qq.com>
2022-11-04 23:58:14 +0800

    wip: transform h call expression

--- swc/jsx.ts
+++ swc/jsx.ts
@@ -1,14 +1,48 @@
-import { transform } from '@swc/core'
-import { Program } from '@swc/core'
+import { JSXOpeningElement, transform } from '@swc/core'
+import { Visitor } from '@swc/core/Visitor'
+import type { Program, JSXElement, CallExpression } from '@swc/core'
 
-const transformVueJsx = (module: Program) => {
-  console.log('module', module)
+const transformTag = (n:JSXOpeningElement) => {
+  const { name } = n
+  if (name.type === 'Identifier') {
+    return {
+      type: 'StringLiteral',
+      value: name.value,
+      hasEscape: false,
+      span: n.span
+    }
+  }
+}
 
-  return module
+const transformVueJsx = (program: Program) => {
+
+  const transformJSXElement = (n:JSXElement) => {
+    console.log('n', n)
+    
+    const tag = transformTag(n.opening)
+    return {
+      type: 'CallExpression',
+      callee: {
+        type: 'Identifier',
+        span: n.span,
+        value: 'h',
+      },
+      span: n.span
+    } as CallExpression
+  }
+
+  class JsxTransformer extends Visitor {
+    visitJSXElement(n: JSXElement): any {
+      const expression = transformJSXElement(n)
+      return expression
+    }
+  }
+
+  program = new JsxTransformer().visitProgram(program)
+  return program
 }
 
 export const transformJsx = async (code:string) => {
-  
   const result =  await transform(code, {
     jsc: {
       target: 'es2022',
