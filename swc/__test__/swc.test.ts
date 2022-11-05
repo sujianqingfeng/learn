5766d9e8284fd6913631181f0c937e5eaf0a3c70
hens <121116111@qq.com>
2022-11-05 23:30:09 +0800

    feat(swc): base jsx

--- swc/__test__/swc.test.ts
+++ swc/__test__/swc.test.ts
@@ -3,9 +3,13 @@ import { transformJsx } from '../jsx'
 
 describe.only('swc', () => {
   it('jsx', async () => {
-    const code = await  transformJsx('const a = <div></div>')
+    const code = await  transformJsx('const a = <div class="b">test</div>')
     expect(code).toMatchInlineSnapshot(`
-      "const a = h(\\"div\\");
+      "const a = h(\\"div\\", {
+          \\"class\\": \\"b\\"
+      }, [
+          \\"test\\"
+      ]);
       "
     `)
   })
--- swc/jsx.ts
+++ swc/jsx.ts
@@ -1,9 +1,27 @@
-import {  StringLiteral, transform } from '@swc/core'
+import { transform } from '@swc/core'
 import { Visitor } from '@swc/core/Visitor'
-import type { Program, JSXElement, CallExpression, Argument, Expression, JSXOpeningElement, Node } from '@swc/core'
+import type {
+  ArrayExpression, 
+  ExprOrSpread, 
+  JSXElementChild,
+  ObjectExpression, 
+  StringLiteral,
+  Property, 
+  Program,
+  JSXElement,
+  CallExpression,
+  Argument,
+  Expression,
+  JSXOpeningElement,
+  Node 
+} from '@swc/core'
 
 const notSupport = (n:Node) => {
   throw new Error(`${n.type} is not support`)
+}
+
+const wrapperExpression = (arr: Expression[]) => {
+  return arr.map((item):Argument => ({ expression: item }))
 }
 
 const transformTag = (n:JSXOpeningElement): StringLiteral => {
@@ -20,8 +38,60 @@ const transformTag = (n:JSXOpeningElement): StringLiteral => {
   }
 }
 
-const wrapperExpression = (arr: Expression[]) => {
-  return arr.map((item):Argument => ({ expression: item }))
+const transformJSXAttributes = (opening:JSXOpeningElement):ObjectExpression | undefined => {
+  if (!opening.attributes) { return }
+
+  const propertyMap:Map<StringLiteral, Expression> = new Map()
+
+  for (const attribute of opening.attributes) {
+    if (attribute .type ==='JSXAttribute') {
+      if (attribute.name.type === 'Identifier') {
+        if (attribute.value?.type === 'StringLiteral') {
+          const key = {
+            type: 'StringLiteral',
+            value: attribute.name.value,
+            span: attribute.name.span
+          } as StringLiteral
+
+          propertyMap.set(key, attribute.value)
+        }
+      }
+    }
+  }
+
+  const properties:Property[] = []
+  for (const [key, value] of  propertyMap.entries()) {
+    const property  = {
+      type: 'KeyValueProperty',
+      key,
+      value
+    } as Property 
+    properties.push(property)
+  }
+
+  return {
+    type: 'ObjectExpression',
+    properties,
+    span: opening.span
+  }
+}
+
+const transformChildren = (children:JSXElementChild[]) => {
+  // console.log('children', children)
+  const list = []
+  
+  for (const child of children) {
+    if (child.type === 'JSXText') {
+      const text =  {
+        type: 'StringLiteral',
+        value: child.value,
+        span: child.span
+      } as StringLiteral
+
+      list.push(text)
+    }
+  }
+  return list
 }
 
 const transformVueJsx = (program: Program) => {
@@ -29,10 +99,32 @@ const transformVueJsx = (program: Program) => {
   const transformJSXElement = (n:JSXElement) => {
     // console.log('n', n)
     
+    // tag
     const tag = transformTag(n.opening)
+
+    // attrs
+    const attrs:Expression[] =[tag]
+    if (n.opening.attributes) {
+      const attributes = transformJSXAttributes(n.opening)
+      if (attributes ) {
+        attrs.push(attributes )
+      }
+    }
+
+    // children
+    const children = transformChildren(n.children)
+
+    if (children.length) {
+      attrs.push({
+        type: 'ArrayExpression',
+        elements: children.map(child => ({ expression: child } as ExprOrSpread)),
+        span: n.span
+      } as ArrayExpression)
+    }
+
     return {
       type: 'CallExpression',
-      arguments: wrapperExpression([tag]),
+      arguments: wrapperExpression(attrs),
       callee: {
         type: 'Identifier',
         span: n.span,
